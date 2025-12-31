5a86f563ca40b182f7904ed05e9fc0e5455dc4a5
Vitor Lazzaretti <112819432+VitorLazzaretti@users.noreply.github.com>
2025-12-31 19:37:51 +0100

    fix

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,25 +1,39 @@
 import json
 import requests
 
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = {"raw_document": { "text": text_to_analyze }}
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+def emotion_detector(input_text):
+    api_url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    payload = {
+        "raw_document": {
+            "text": input_text
+        }
+    }
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
 
-    formatted_response = json.loads(response.text)
+    api_response = requests.post(api_url, json=payload, headers=headers)
+    parsed_response = json.loads(api_response.text)
 
-    emotion_scores = {}
-    if response.status_code == 400:
-        emotion_scores =  {'anger':None,'disgust':None,'fear':None,'joy':None,'sadness':None,'dominant_emotion':None}
-        return emotion_scores
+    results = {}
 
-    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    if api_response.status_code == 400:
+        results = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        return results
 
-    emotion_scores['dominant_emotion'] = dominant_emotion
+    results = parsed_response['emotionPredictions'][0]['emotion']
+    main_emotion = max(results, key=results.get)
+    results['dominant_emotion'] = main_emotion
 
-    return emotion_scores
+    return results
+
 
 text_to_analyze = "I love this new technology!"
 text = emotion_detector(text_to_analyze)
